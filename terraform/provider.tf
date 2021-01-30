a61413f0c2967b81a6af2812705394112ae0a474
Ravindra Singh <51900358+ravindrasinghh@users.noreply.github.com>
2021-01-30 16:48:45 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,10 +4,12 @@ terraform {
   }
   backend "gcs" {
     bucket = "test-terraform-01"
+    credentials = "/home/ubuntu/terraform.json"
   }
 }
 
 provider "google" {
+  credentials = "/home/ubuntu/terraform.json"
   project     = "kinetic-guild-274011"
   region      = "asia-east1"
 }
